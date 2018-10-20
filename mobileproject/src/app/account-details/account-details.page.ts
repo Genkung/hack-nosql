d0f499671f2ccce35829dd9b4933230d6f44f899
nanha101 <nanppw605@gmail.com>
2018-10-20 16:57:00 +0700

    update accoutdetails

--- mobileproject/src/app/account-details/account-details.page.ts
+++ mobileproject/src/app/account-details/account-details.page.ts
@@ -6,6 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./account-details.page.scss'],
 })
 export class AccountDetailsPage implements OnInit {
+  
+  Account_data:any={Username :"John smith",Coins:[],Balance:400.0};
+  Account_Coins_data:any=[{Symbol:"BTH",Quantity:80.5,TotalAmount:"700"},
+                          {Symbol:"BTC",Quantity:81.5,TotalAmount:"800"},
+                          {Symbol:"BT",Quantity:82.5,TotalAmount:"900"}];
+  balance=0.0;
+    // Account_Data:any=[{
+    //     _id:"1",
+    //     symbol:"BTC",
+    //     buy:20.5,
+    //     sell:60.3
+    //   },
+    //   {
+    //     _id:"2",
+    //     symbol:"THB",
+    //     buy:80.36,
+    //     sell:98.30
+    //   },
+    // ];
 
   constructor() { }
 
